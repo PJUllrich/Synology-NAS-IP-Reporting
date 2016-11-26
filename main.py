85ab1a5aa6e508f06b8e36a443b6a75d49892dbf
Peter Ullrich <peter.j.ullrich@web.de>
2016-11-26 18:49:11 +0100

    Changed the SQLite query to make it safer against injections. Don’t know exactly why I did that but hey, why not.

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ dbpath = '/etc/synoautoblock.db'
 conn = sqlite3.connect(dbpath)
 c = conn.cursor()
 
-c.execute('SELECT IP FROM AutoBlockIP WHERE Deny=1')
+c.execute('SELECT {c} FROM {t} WHERE {cc}=1')\
+    .__format__(c='IP', t='AutoBlockIP', cc='Deny')
+
 result = c.fetchall()
 
 textFile = open("output.txt", "w")
