6ed5aa8d26724b968b57d48596c19ee60dcf47ab
Peter Ullrich <peter.j.ullrich@web.de>
2016-11-29 14:53:41 +0100

    Added connection to DB and writing the result to a text file.

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import sqlite3
+import requests
 
-dbpath = '/etc/synoautoblock.db'
+confPath = 'conf.txt'
+dbPath = '/etc/synoautoblock.db'
+apiURL = 'https://www.abuseipdb.com/report/json'
+apiKey = 'YOUR API KEY HERE'
+apiCategories = '18,20'
+apiComment = 'Tried to login into MailServer without Permission. Got blocked after 3 tries.'
 
-conn = sqlite3.connect(dbpath)
+with open(confPath) as f:
+    lastIP = f.readline()
+
+conn = sqlite3.connect(dbPath)
 c = conn.cursor()
 
-c.execute('SELECT {c} FROM {t} WHERE {cc}=1')\
-    .__format__(c='IP', t='AutoBlockIP', cc='Deny')
+c.execute('SELECT IP FROM AutoBlockIP WHERE Deny=1')
 
 result = c.fetchall()
 
 textFile = open("output.txt", "w")
-textFile.write(result)
+
+for item in result:
+    textFile.write("%s\n" % item)
+
 textFile.close()
 
 conn.close()
